7abee4043e9351e64221ef4152fc93ecdeb449ea
Michael Kimsal <mgkimsal@gmail.com>
2011-10-17 11:56:56 -0400

    initial push

---
+++ closures/dynamic_closures.php
@@ -0,0 +1,41 @@
+<?php
+abstract class mgkbase {
+
+	public $__closures = array();
+	public $name = "sample\n";
+
+	public function __set($name, $val) {
+		if($val instanceof closure)
+		{
+			$this->__closures[$name] = $val;
+		}
+	}
+
+	public function __get($name)
+	{
+		if(array_key_exists($name, $this->__closures))
+		{
+			return $this->__closures[$name];
+		}
+	}
+
+	public function __call($name, $args)
+	{
+		if(array_key_exists($name, $this->__closures))
+		{
+			$c = $this->__closures[$name];
+			$c($args);
+		}
+	}
+
+}
+
+class foo extends mgkbase{}
+
+$y = new foo();
+$y->showNow = function() use ($y) { 
+	echo $y->name;
+	echo "now=".time()."\n";
+};
+
+$y->showNow();
